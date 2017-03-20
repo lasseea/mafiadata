20429cb7439ff1a1f986dae4610ada150a3859ae
lasseea <leadk90@gmail.com>
2017-03-20 18:53:05 +0100

    added model classes
    added model classes for all current database migration tables. Still
    needs specification of tables they are the model of and declaration of
    the fillable array of table values

---
+++ app/Player_slot_player.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Player_slot_player extends Model
+{
+    //
+}
---
+++ app/User_community_allowance.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User_community_allowance extends Model
+{
+    //
+}
